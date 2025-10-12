29c0a37476cbd01baecd00bfd35471f7390e9c4c
Neeraj Jakhar <31380679+slashblog@users.noreply.github.com>
2025-10-12 20:56:30 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -7,13 +7,11 @@ DIST_DIR="dist"
 
 npm run compile:schemas
 npm run build
-ls -l .
 mkdir -p "$DIST_DIR"
-ls -l .
 
 cd "$DIST_DIR"
 rm -rf metadata.json icons schemas flags
-cp -r ../src/{metadata.json,stylesheet.css,schemas,icons} ../assets/flags .
+cp -r ../src/metadata.json ../src/stylesheet.css ../src/schemas ../src/icons ../assets/flags .
 
 rm -f "$ZIP_FILE"
 zip -qr "$ZIP_FILE" *.js *.css metadata.json icons flags schemas
